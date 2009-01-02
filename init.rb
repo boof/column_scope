bef057bad6ab03bc08e1b52a3c85f984694f765a
Florian Aßmann <florian.assmann@email.de>
2009-01-02 13:46:11 +0100

    Documentation fixes.

--- init.rb
+++ init.rb
@@ -1,15 +1,16 @@
 require "#{ File.dirname __FILE__ }/lib/active_record/named_scope/column_scope"
 
-ActiveRecord::Base.class_eval do
-  extend ActiveRecord::NamedScope::ColumnScope::ScopeMethods
+module ActiveRecord
 
-  def self.values
-    ActiveRecord::NamedScope::ColumnScope::
-        ValueExtractor.new self, column_names
+  Base.extend NamedScope::ColumnScope::ScopeMethods
+  # Overwrite <tt>Base.values</tt> to directly call <tt>column_names</p>.
+  def Base.values
+    NamedScope::ColumnScope::ValueExtractor.new self, column_names
+  end
+
+  module NamedScope
+    Scope.class_eval { include ColumnScope::ScopeMethods }
   end
 
 end
 
-ActiveRecord::NamedScope::Scope.class_eval do
-  include ActiveRecord::NamedScope::ColumnScope::ScopeMethods
-end
